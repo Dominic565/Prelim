21396e34e24097d30a5a0d30b26c85e70aeab843
DominicD <diezd222@gmail.com>
2021-09-20 15:28:58 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Prelim Project</h1>
+   <div class="flex items-center">
+   <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://127.0.0.1:8000/hello" class="underline text-gray-900 dark:text-white">Documentation</a></div>
+   </div>
+   <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://127.0.0.1:8000/contact" class="underline text-gray-900 dark:text-white">Contact</a></div>
+   <div class="ml-4 text-lg leading-7 font-semibold"><h1><a href="http://127.0.0.1:8000/about" class="underline text-gray-900 dark:text-white">About
+</a></h1></div>
+  <h1>About</h1>
+     
+</body>
+</html>
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getlocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Hello</title>
+   
+</head>
+<body>
+   <h1>Prelim Project</h1>
+
+     <div> 
+       <nav class="d-flex aligns-item justify-conten-center" style="height:150px">
+       <div class="d-flex list-unstyled">
+       <div class="ml-4 text-lg leading-7 font-semibold"><h1><a href="http://127.0.0.1:8000/hello" class="underline text-gray-900 dark:text-white">Documentation</a></h1></div>
+       <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://127.0.0.1:8000/contact" class="underline text-gray-900 dark:text-white">Contact</a></div>
+       <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://127.0.0.1:8000/about" class="underline text-gray-900 dark:text-white">About</a></div>
+       </div>
+       </nav>
+      </div>
+      
+  <h1>Documentation</h1>
+  
+</body>
+</html>
